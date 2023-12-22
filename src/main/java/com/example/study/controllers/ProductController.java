dbe36d435613be325e5a3cd0edc3c0f2325d2ee7
kkozoriz <kkozoriz7@yandex.ru>
2023-12-22 17:06:53 +0300

    add methods create/delete/info product to ProductController

--- src/main/java/com/example/study/controllers/ProductController.java
+++ src/main/java/com/example/study/controllers/ProductController.java
@@ -1,13 +1,42 @@
 package com.example.study.controllers;
 
+import com.example.study.models.Product;
+import com.example.study.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.logging.Logger;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
-    @GetMapping("/")
-    public String getProducts() {
+    private final ProductService productService;
 
+    @GetMapping("/")
+    public String getProducts(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProducts(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
     }
 }
